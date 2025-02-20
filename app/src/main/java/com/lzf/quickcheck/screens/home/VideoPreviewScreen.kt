52a92e5d778824918a84b0600c392d59811f918d
LvZhifeng <1490698296@163.com>
2025-02-20 18:25:12 +0800

    实时监测加入一点细节
    待完成

--- app/src/main/java/com/lzf/quickcheck/screens/home/VideoPreviewScreen.kt
+++ app/src/main/java/com/lzf/quickcheck/screens/home/VideoPreviewScreen.kt
@@ -1,7 +1,71 @@
 package com.lzf.quickcheck.screens.home
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.camera.core.CameraSelector
+import androidx.camera.video.Recorder
+import androidx.camera.video.Recording
+import androidx.camera.video.VideoCapture
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.navigation.NavController
+import com.lzf.quickcheck.R
 
+
+@RequiresApi(Build.VERSION_CODES.P)
 @Composable
-fun  VideoPreviewScreen(navController: NavController) { }
+fun  VideoPreviewScreen(navController: NavController) {
+
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    var recording: Recording? = remember { null }
+    val previewView: PreviewView = remember { PreviewView(context) }
+    val videoCapture: MutableState<VideoCapture<Recorder>?> = remember { mutableStateOf(null) }
+    val recordingStarted: MutableState<Boolean> = remember { mutableStateOf(false) }
+
+    val audioEnabled: MutableState<Boolean> = remember { mutableStateOf(false) }
+    val cameraSelector: MutableState<CameraSelector> = remember {
+        mutableStateOf(CameraSelector.DEFAULT_BACK_CAMERA)
+    }
+
+    LaunchedEffect(previewView) {
+        videoCapture.value = context.createVideoCaptureUseCase(
+            lifecycleOwner = lifecycleOwner,
+            cameraSelector = cameraSelector.value,
+            previewView = previewView
+        )
+    }
+
+    AndroidView(
+        factory = { previewView },
+        modifier = Modifier.fillMaxSize()
+    )
+    IconButton(
+        onClick = {
+
+        },
+
+    ) {
+        Icon(
+            painter = painterResource(if (recordingStarted.value) R.drawable.ic_stop else R.drawable.ic_record),
+            contentDescription = "",
+            modifier = Modifier.size(64.dp)
+        )
+    }
+
+}
